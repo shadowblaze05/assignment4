d3f2a7fb6483618faa83ea56e8b09a8fc415944a
shadowblaze05 <shadowblaze259@gmail.com>
2025-10-06 22:05:55 +0800

    New files for Assignment 4

---
+++ pages/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'body']
+        
+    
+    def clean_title(self):
+        t = self.cleaned_data['title']
+        if "test" in t.lower():
+            raise forms.ValidationError("Title cannot contain the word 'test'")
+        if len(t) < 3:
+            raise forms.ValidationError("Title is short like you")
+        return t
---
+++ pages/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseBadRequest
+from django.contrib import messages
+from pages.models import Post
+from pages.forms import PostForm
+from django.shortcuts import redirect
+
+
+# Create your views here.
+def home(request):
+    ctx = {"title": "Home", "features": ["Django", "Templates", "Static files"]}
+    return render(request, "home.html", ctx)
+
+def about(request):
+    return render(request, "about.html", {"title": "About"})
+
+def hello(request, name):
+    return render(request, "hello.html", {"name": name})
+
+def gallery(request):
+    # Assume images placed in pages/static/img/
+    images = ["img1.jpg", "img2.jpg", "img3.jpg"]
+    return render(request, "gallery.html", {"images": images})
+
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
+
+def server_error_view(request):
+    return render(request, '500.html', status=500)
+
+def post_list(request):
+    # Model.objects.all()
+    posts = Post.objects.all().prefetch_related('comments')
+    context = {
+        'posts': posts,
+        'title': 'Posts',
+    }
+    return render(request, 'post_list.html', context)
+
+def admin(request):
+    return redirect('/admin/')
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'New post created')
+            return redirect('post_list')
+        messages.error(request, 'Get Better!')
+    else:
+        form = PostForm()
+    return render(request, 'post_form.html', {'form': form})
+
+def post_view(request, pk):
+    #post = get_object_or_404(Post, pk=pk)
+    post = Post.objects.get(pk=pk)
+    comments = Post.objects.prefetch_related('comments')
+    return render(request, 'post_view.html', {'post': post})
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'New post created')
+            return redirect('post_list')
+        messages.error(request, 'Get Better!')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_form.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        messages.success(request, f"Goodbye {post.title} ")
+        return redirect('post_list')
+    return render(request, 'post_confirm_delete.html', {'post': post})
